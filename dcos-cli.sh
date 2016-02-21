33670827da44cdcaf5dba61ae213cb2eb676925b
cloud-init created default user <ec2-user@ip-172-31-20-248.ap-southeast-1.compute.internal>
2016-02-21 08:31:45 +0000

    use abs-path and make package update optional

--- dcos-cli.sh
+++ dcos-cli.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ "${1}" = "--pkg-update" ]; then
+    /dcos-cli/bin/dcos package update
+    shift
+fi
 /bin/bash /dcos-cli/config.sh
-dcos-cli/bin/dcos package update
-dcos-cli/bin/dcos "$@"
+/dcos-cli/bin/dcos "$@"
